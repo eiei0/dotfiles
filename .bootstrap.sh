42b6527ebaa16d41c0c938e4090cbad3590ebe76
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2023-04-27 01:27:52 -0500

    Abstract string formatters into reusable file

--- .bootstrap.sh
+++ .bootstrap.sh
@@ -1,42 +1,6 @@
 #!/bin/bash
 
-##### String Formatters #####
-
-if [[ -t 1 ]]
-then
-  tty_escape() { printf "\033[%sm" "$1"; }
-else
-  tty_escape() { :; }
-fi
-tty_mkbold() { tty_escape "1;$1"; }
-tty_blue="$(tty_mkbold 34)"
-tty_red="$(tty_mkbold 31)"
-tty_bold="$(tty_mkbold 39)"
-tty_reset="$(tty_escape 0)"
-
-shell_join() {
-  local arg
-  printf "%s" "$1"
-  shift
-  for arg in "$@"
-  do
-    printf " "
-    printf "%s" "${arg// /\ }"
-  done
-}
-
-chomp() {
-  printf "%s" "${1/"$'\n'"/}"
-}
-
-ohai() {
-  printf "${tty_blue}==>${tty_bold} %s${tty_reset}\n" "$(shell_join "$@")"
-}
-
-warn() {
-  printf "${tty_red}Warning${tty_reset}: %s\n" "$(chomp "$1")" >&2
-}
-
+source $HOME/.str_fmt.sh
 
 ##### Shared Functions #####
 
