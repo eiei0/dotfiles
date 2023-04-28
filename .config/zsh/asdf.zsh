dd742c45d2dd947bb706d23969744248ee06fa3d
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2023-04-27 21:08:37 -0500

    Change ZSH_CUSTOM directory and add fzf/asdf configs

---
+++ .config/zsh/asdf.zsh
@@ -0,0 +1,3 @@
+# ================= asdf configs ==================================================================
+
+. $(brew --prefix asdf)/libexec/asdf.sh                                          # load asdf script
