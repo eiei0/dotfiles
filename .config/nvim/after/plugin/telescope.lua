4fd91096a239c9b6505ff6902b8d1aa6152dba6d
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2023-04-29 22:47:57 -0500

    Remap up/down nav in telescope

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,4 +1,18 @@
-local builtin = require('telescope.builtin')
+local telescope = require("telescope")
+local builtin = require("telescope.builtin")
+local actions = require("telescope.actions")
 
-vim.keymap.set('n', ',t', builtin.find_files, {})  -- fuzzy finder for all files 
+vim.keymap.set('n', ',t', builtin.find_files, {})  -- fuzzy finder for all files
 vim.keymap.set('n', ',f', builtin.git_files, {}) -- fuzzy finder for files tracked with git
+
+telescope.setup {
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+        ["<esc>"] = actions.close
+      }
+    }
+  }
+}
