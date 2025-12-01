a2661f2a49c48293af7fc9a4b03be1ca651ecbab
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2025-12-01 10:02:29 -0600

    Add lsp configs for gopls

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -14,6 +14,29 @@ config.solargraph.setup {
   filetypes = { 'ruby', 'rspec' },
 }
 
+-- Optimize gopls for better performance
+config.gopls.setup {
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+        shadow = false,
+      },
+      staticcheck = false,
+      gofumpt = true,
+      hints = {
+        assignVariableTypes = false,
+        compositeLiteralFields = false,
+        compositeLiteralTypes = false,
+        constantValues = false,
+        functionTypeParameters = false,
+        parameterNames = false,
+        rangeVariableTypes = false,
+      },
+    },
+  },
+}
+
 require('go').setup()
 
 lsp.setup()
