0caabb63cfabe7d0f9d667fab96141baab57cabb
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2025-12-01 12:58:09 -0600

    Fix keymaps to use the newer vim.keymap.set() API

--- .config/nvim/after/plugin/refactoring.lua
+++ .config/nvim/after/plugin/refactoring.lua
@@ -1,9 +1,9 @@
 require('refactoring').setup({})
 
 require("telescope").load_extension("refactoring")
-vim.api.nvim_set_keymap(
+vim.keymap.set(
 	"v",
 	"<leader>rr",
 	"<Esc><cmd>lua require('telescope').extensions.refactoring.refactors()<CR>",
-	{ noremap = true }
+	{ noremap = true, desc = "Open refactoring menu" }
 )
