5e43a3662bc5b686047c714be37cd99bf55cd666
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2025-12-01 12:59:08 -0600

    Remove telescope verion pin

--- .config/nvim/lua/eiei0/lazy.lua
+++ .config/nvim/lua/eiei0/lazy.lua
@@ -22,7 +22,6 @@ require("lazy").setup({
   "jeffkreeftmeijer/vim-numbertoggle",
   {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.6",
     dependencies = { "nvim-lua/plenary.nvim" }
   },
   {
