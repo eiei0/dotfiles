860ccb2ce0c7e6f7dd6064d25713c58c4eb79b55
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2025-12-01 09:48:27 -0600

    Migrate to lazy.nvim

--- .config/nvim/lua/eiei0/init.lua
+++ .config/nvim/lua/eiei0/init.lua
@@ -1,2 +1,6 @@
+-- Plugin manager (uncomment one)
+require("eiei0.lazy")     -- lazy.nvim (default)
+-- require("eiei0.packer") -- packer.nvim (legacy - also need to run :PackerSync after uncommenting)
+
 require("eiei0.set")
 require("eiei0.remap")
---
+++ .config/nvim/lua/eiei0/lazy.lua
@@ -0,0 +1,85 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugin specifications
+require("lazy").setup({
+  -- Navigation
+  "github/copilot.vim",
+  "preservim/nerdtree",
+  "tpope/vim-surround",
+  "jeffkreeftmeijer/vim-numbertoggle",
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.6",
+    dependencies = { "nvim-lua/plenary.nvim" }
+  },
+  {
+    "kelly-lin/telescope-ag",
+    dependencies = { "nvim-telescope/telescope.nvim" }
+  },
+  "christoomey/vim-tmux-navigator",
+  "folke/trouble.nvim",
+
+  -- Colors
+  {
+    "morhetz/gruvbox",
+    config = function()
+      vim.cmd.colorscheme("gruvbox")
+    end
+  },
+  "nvim-lualine/lualine.nvim",
+
+  -- Syntax
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate"
+  },
+  {
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v2.x",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      {
+        "williamboman/mason.nvim",
+        build = ":MasonUpdate"
+      },
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/nvim-cmp",
+      "hrsh7th/cmp-nvim-lsp",
+      "L3MON4D3/LuaSnip",
+    }
+  },
+  "tpope/vim-commentary",
+  "derekprior/vim-trimmer",
+  "jose-elias-alvarez/null-ls.nvim",
+  "j-hui/fidget.nvim",
+
+  -- Git
+  "lewis6991/gitsigns.nvim",
+  "tpope/vim-fugitive",
+
+  -- Ruby/Rails
+  "keith/rspec.vim",
+
+  -- Golang
+  "ray-x/go.nvim",
+
+  -- Misc
+  "vim-test/vim-test",
+  {
+    "ThePrimeagen/refactoring.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter"
+    }
+  },
+})
